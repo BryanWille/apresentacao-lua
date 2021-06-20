f886d79953a2cdda9cc5700040943df4d3360a11
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-06-19 21:35:01 -0300

    Update ex7c6.lua

--- ex7c6.lua
+++ ex7c6.lua
@@ -1,13 +1,11 @@
---        Entrada de Dados
+valorFixo = perc = 0
 
-io.write("Digite seu nome: ")   -- Classe io, metódo write para escrever.
-nome = io.read()    --<etódo de leitura, o tipo deve ser escrito entre parênteses (padrão é String)
+io.write("Digite seu nome: ")   
+nome = io.read() 
 io.write("Digite o salario de ", nome, ": ")
-salario = io.read("*n") -- "*n" indica que é um número inteiro
+salario = io.read("*n")
 
---        Comando IF
-
-if salario < 300 then   -- Todo if e elseif precisa de um then (ou seja um então)
+if salario < 300 then  
   perc = 0.70
   valorFixo = 500
 elseif salario < 1000 then
@@ -16,14 +14,10 @@ elseif salario < 1000 then
 else 
   perc = 0.30
   valorFixo = 0 
-end     -- End pra indicar que acabou o if.
-
---         Operações
+end   
 
 plBruto = valorFixo + (salario * perc)
 impostoRenda = plBruto * 0.25
 plLiquido = plBruto - impostoRenda
 
---         Saída de Dados
-
-io.write("Participacaoo nos lucros liquido de ", nome, " foi de: R$", plLiquido) 
+io.write("Participacaoo nos lucros liquido de ", nome, " foi de: R$", plLiquido)
